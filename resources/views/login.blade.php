0c11219a9c88218a620f0ffa8c2e2bf21d6fa28a
Prajanya Shrestha <Prajanya.stha7@gmail.com>
2022-05-14 13:15:13 +0545

    improved login form UI using just HTML

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,7 @@
+<h1>User Login</h1>
 <form action="/login" method="post">
   @csrf
-  <input type="email" name="email">
-  <input type="password" name="password">
-  <input type="submit" value="Submit">
+  <input type="email" name="email" placeholder="Enter email"> <br><br>
+  <input type="password" name="password" placeholder="Enter password"> <br><br>
+  <button type="submit">Login</button>
 </form>
