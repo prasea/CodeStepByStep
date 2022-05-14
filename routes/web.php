5074d46a08b71a79161e929f1d55fc63e75ecba2
Prajanya Shrestha <Prajanya.stha7@gmail.com>
2022-05-14 19:15:46 +0545

    Adding more routes to grouped m/w

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::view('home', 'home');
 Route::view('noaccess', 'noaccess');
 
 Route::group(['middleware' => ['protectedPage']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::view('home', 'home');
     Route::view('user', 'user');
 });
