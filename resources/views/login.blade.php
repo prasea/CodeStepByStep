aed0bc53ba5ef6fa01669b18370c1e3d648cc192
Prajanya Shrestha <Prajanya.stha7@gmail.com>
2022-05-15 09:24:47 +0545

    Route and view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,7 @@
+<form action="">
+  <input type="text" name="username" placeholder="Enter username">
+  <br><br>
+  <input type="password" name="password" placeholder="Enter password">
+  <br><br>
+  <button type="submit">Login</button>
+</form>
