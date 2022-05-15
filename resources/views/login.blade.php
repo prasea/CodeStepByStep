125d59fee76fa09419d9bdccd7eb8be63f2815ba
Prajanya Shrestha <Prajanya.stha7@gmail.com>
2022-05-15 08:46:08 +0545

    Added login route and  view without controller

---
+++ resources/views/login.blade.php
@@ -0,0 +1,5 @@
+<form action="">
+  <input type="text" name="user" placeholder="Enter username"> <br><br>
+  <input type="password" name="pw" id="pw" placeholder="Enter password"> <br><br>
+  <button type="submit">Login</button>
+</form>
